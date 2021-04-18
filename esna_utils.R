1f91598ba23a1bfb81eb01506ee0ab63e90f0440
David Klotz <david.klotz@pressganey.com>
2021-04-18 12:28:31 -0400

    Many new commits (4/18)

--- esna_utils.R
+++ esna_utils.R
@@ -2,9 +2,25 @@ library(dplyr)
 library(stringr)
 library(textclean)
 
+
+# replaceTranslit = function() {
+#   
+#   context <- rstudioapi::getActiveDocumentContext()
+#   
+#   for (sel in context$selection) {
+#     
+#     txt  <- sel$text
+#     
+#     newtxt <- translit(txt)
+#     
+#     rstudioapi::modifyRange(sel$range, as.character(newtxt), context$id)
+#     
+#   }
+# }
+
 bookdown::render_book(input = ".")
 bookdown::publish_book(name = "Esna2", account = "shemanefer", server = "bookdown.org")
-
+rmarkdown::find_pandoc(dir = "c://users/klotzd/AppData/local/pandoc/", version = "2.13.0")
 
 fix_translit <- function(x) {
   
@@ -13,25 +29,43 @@ fix_translit <- function(x) {
 
 }
 
+tmp_theme <- function() bookdown::bs4_book(theme = bookdown::bs4_book_theme(font_scale = .5, 
+                                                                            primary = "#9B110E"))
+translate <- function(x){
+ 
+    stringr::str_split(x, "\\n") %>% 
+    unlist %>% 
+    #paste0("| ", .) %>% 
+    paste(collapse = "  \n") %>% 
+    cat
+    
+}
 translit <- function(x){
   
   fix_translit(x) %>% 
     stringr::str_split(., "\\n") %>% 
     unlist %>% 
-    paste0("| *", ., "*  ") %>% 
+    paste0("*", ., "*  ") %>% 
     stringi::stri_replace_all_fixed("*  ", "  *") %>% 
     stringi::stri_replace_all_fixed("*  ", "  *") %>% 
     stringi::stri_replace_all_fixed("*  ", "  *") %>% 
-    paste(collapse = "\n") %>% 
-    cat
+    stringi::stri_replace_all_fixed("*  ", "  *") %>% 
+    paste(collapse = "  \n") 
   
   
 }
 
-translit("nsw.t-bity
-nTr nTry HqA pAwty.w
-ity nb nTr.w rmT
-nxt wr-pHty
-smA bdS
-ir anD m aApp
-Xnm-Ra nb sx.t")
+translit("| *qȝ pȝwty.w  *
+| *ỉty nb nṯr.w rmṯ  *
+| *nḫt wr-pḥty  *
+| *smȝ bdš  *
+| *ỉr ʿnḏ m ʿȝpp  *
+| *ẖnm-Rʿ nb sḫ.t  *")
+
+| *nsw.t-bỉty  *
+| *nṯr nṯry ḥqȝ pȝwty.w  *
+| *ỉty nb | *nṯr  *.w rmṯ  *
+| *nḫt wr-pḥty  *
+| *smȝ bdš  *
+| *ỉr ʿnḏ m ʿȝpp  *
+| *ẖnm-Rʿ nb sḫ.t  *
